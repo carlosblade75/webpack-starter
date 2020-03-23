96bf484e8104f545361c58a63da36086eb5d3760
carlosblade75 <carlosblade75@hotmail.com>
2020-03-23 17:36:43 +0100

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { saluda } from './js/componentes.js';
+import './styles.css';
+
+saluda('Carlos Siera');
---
+++ src/js/componentes.js
@@ -0,0 +1,12 @@
+import '../css/componentes.css';
+
+export const saluda = ( nombre) =>{
+
+    console.log(nombre);
+
+    const h1 = document.createElement('h1');
+
+    h1.innerText = `Hola, ${nombre}`;
+
+    document.body.append(h1);
+};
